ccded9c1f35a1626a47b49820d9efa127a22762a
fabien beaujean <fabienbeaudimi@hotmail.fr>
2016-12-27 02:42:51 +0100

    update database schema

--- pib.sql
+++ pib.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 24 Décembre 2016 à 17:48
+-- Généré le :  Mar 27 Décembre 2016 à 01:42
 -- Version du serveur :  5.7.9
 -- Version de PHP :  7.0.0
 
@@ -23,6 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `creation`
+--
+
+DROP TABLE IF EXISTS `creation`;
+CREATE TABLE IF NOT EXISTS `creation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `video` int(11) NOT NULL,
+  `subtitle` int(11) NOT NULL,
+  `music` int(11) NOT NULL,
+  `score` float NOT NULL DEFAULT '0',
+  `count` int(11) NOT NULL DEFAULT '0',
+  `sum` float DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `creation_video_id_fk` (`video`),
+  KEY `creation_subtitle_id_fk` (`subtitle`),
+  KEY `creation_music_id_fk` (`music`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `creation`
+--
+
+INSERT INTO `creation` (`id`, `title`, `video`, `subtitle`, `music`, `score`, `count`, `sum`) VALUES
+(1, 'Création d''exemple 1', 1, 1, 1, 4, 1, 4),
+(2, 'Création d''exemple 2', 1, 1, 1, 5, 1, 5);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `music`
 --
 
@@ -33,7 +63,14 @@ CREATE TABLE IF NOT EXISTS `music` (
   `duration` int(11) NOT NULL,
   `path` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `music`
+--
+
+INSERT INTO `music` (`id`, `title`, `duration`, `path`) VALUES
+(1, 'Musique d''exemple', 167, '/web/pib/file/music.mp3');
 
 -- --------------------------------------------------------
 
@@ -47,7 +84,8 @@ CREATE TABLE IF NOT EXISTS `subelement` (
   `time` int(11) NOT NULL,
   `content` text NOT NULL,
   `subtitle` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `subelement_subtitle_id_fk` (`subtitle`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -61,8 +99,16 @@ CREATE TABLE IF NOT EXISTS `subtitle` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `video` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `subtitle_video_id_fk` (`video`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `subtitle`
+--
+
+INSERT INTO `subtitle` (`id`, `title`, `video`) VALUES
+(1, 'Sous-titres d''exemple', 1);
 
 -- --------------------------------------------------------
 
@@ -76,8 +122,40 @@ CREATE TABLE IF NOT EXISTS `video` (
   `title` varchar(255) NOT NULL,
   `duration` int(11) NOT NULL,
   `path` varchar(255) NOT NULL,
+  `poster` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `video`
+--
+
+INSERT INTO `video` (`id`, `title`, `duration`, `path`, `poster`) VALUES
+(1, 'Video d''exemple', 119, '/web/pib/file/video.mp4', '/web/pib/img/static/video-poster.png');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `creation`
+--
+ALTER TABLE `creation`
+  ADD CONSTRAINT `creation_music_id_fk` FOREIGN KEY (`music`) REFERENCES `music` (`id`),
+  ADD CONSTRAINT `creation_subtitle_id_fk` FOREIGN KEY (`subtitle`) REFERENCES `subtitle` (`id`),
+  ADD CONSTRAINT `creation_video_id_fk` FOREIGN KEY (`video`) REFERENCES `video` (`id`);
+
+--
+-- Contraintes pour la table `subelement`
+--
+ALTER TABLE `subelement`
+  ADD CONSTRAINT `subelement_subtitle_id_fk` FOREIGN KEY (`subtitle`) REFERENCES `subtitle` (`id`);
+
+--
+-- Contraintes pour la table `subtitle`
+--
+ALTER TABLE `subtitle`
+  ADD CONSTRAINT `subtitle_video_id_fk` FOREIGN KEY (`video`) REFERENCES `video` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
